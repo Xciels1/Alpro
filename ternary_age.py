2c5a2c906f52c9f02f047a3080c4aa79f8b32f4c
Rico Lanang Ona <146321312+Xciels1@users.noreply.github.com>
2025-02-28 22:37:49 +0700

    Create ternary_age.py

---
+++ ternary_age.py
@@ -0,0 +1,3 @@
+umur = int(input("Masukkan Usia Anda\n>>"))
+
+usia = print("Bayi") if umur <= 5 else print("anak-anak") if umur >= 6 and umur <= 11 else print("ramaja") if umur >= 12 and umur <= 25 else print("dewasa") if umur >=26 and umur <= 45 else print("lansia")
