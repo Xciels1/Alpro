4bc30e311c5dbcc9daa453a806efaf41dadb5be9
Rico Lanang Ona <146321312+Xciels1@users.noreply.github.com>
2025-02-28 22:23:05 +0700

    Create hari_dalam_satu_bulan.py

---
+++ hari_dalam_satu_bulan.py
@@ -0,0 +1,22 @@
+while True:
+  try:
+    bulan = int(input("Masukkan bulan(1-12) : "))
+    jumlah_hari = 31
+
+    if 1 <= bulan <= 12:
+        break
+    else:
+      print("bulan antara (1-12)")
+
+  except Exception as e:
+    print(e)
+    print("bulan harus berupa angka")
+
+if bulan % 2 == 0 and bulan != 2:
+    jumlah_hari = jumlah_hari-1
+    print(jumlah_hari)
+elif bulan == 2:
+    jumlah_hari = jumlah_hari - 3
+    print(jumlah_hari)
+
+print("jumlah hari:", jumlah_hari)
