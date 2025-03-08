a2f86d21e49b31f8c10ce4f4aa2fbc0872026748
Rico Lanang Ona <146321312+Xciels1@users.noreply.github.com>
2025-03-08 21:15:12 +0700

    Update konversiSuhu.py

--- konversiSuhu.py
+++ konversiSuhu.py
@@ -1,4 +1,4 @@
-
+#NO LAMBDA
 konversi = int(input("Konversi Ke\n1.Fahrenheit\n2.Reamur\n>>"))
 while konversi < 1 or konversi > 2:
   print("input todak valid")
@@ -21,3 +21,22 @@ else:
    
      return f"R = {reamur}"
     print(konversi_Reamur()) 
+
+#WITH LAMBDA
+konversi = int(input("1. Suhu Celcius ke Fahrenheit\n2. Suhu Fahrenheit ke Celcius\n>>"))
+
+while konversi < 1 or konversi > 2:
+  print("Inputan tidak valid")
+  konversi = int(input("1. Suhu Celcius ke Fahrenheit\n2. Suhu Fahrenheit ke Celcius\n>>"))
+
+if konversi == 1:
+  print("Konversi Suhu Celcius ke Fahrenheit")
+  celcius = float(input("Masukkan suhu dalam Celcius : "))
+  fahrenheit = lambda celcius : (celcius * 9/5) + 32
+  print("Suhu dalam Fahrenheit adalah", fahrenheit(celcius))
+
+else:
+  print("Konversi Suhu Celcius  ke reamur")
+  celcius = float(input("Masukkan suhu dalam celcius : "))
+  reamur = lambda celcius : 0.8 * celcius
+  print("Suhu dalam reamur adalah", reamur(celcius))
