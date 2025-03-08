c8da8fb910546a089d9a7d42aa66245a111e08b3
Rico Lanang Ona <146321312+Xciels1@users.noreply.github.com>
2025-03-08 20:57:44 +0700

    Update digitBelakang.py

--- digitBelakang.py
+++ digitBelakang.py
@@ -1,3 +1,4 @@
+#1
 def cek_digit_belakang():
   angka1 = int(input("Masukkan angka pertama : "))
   angka2 = int(input("Masukkan angka kedua : "))
@@ -14,3 +15,24 @@ def cek_digit_belakang():
     return False
 
 print(cek_digit_belakang())
+
+#2
+def cek_digit_belakang(angka1, angka2, angka3):
+
+  hasil1 = angka1 % 10
+  hasil2 = angka2 % 10
+  hasil3 = angka3 % 10
+
+  if hasil1 == hasil2 or hasil1 == hasil3 or hasil2 == hasil3:
+    return True
+  
+  else:
+    return False
+
+#TEST CASE
+
+# print(cek_digit_belakang(30, 20, 18)) true
+# print(cek_digit_belakang(145, 5, 100)) true
+# print(cek_digit_belakang(71, 187, 18)) false
+# print(cek_digit_belakang( 1024, 14, 94)) true
+# print(cek_digit_belakang( 53, 8900, 658)) false
